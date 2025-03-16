dd3643105498fce22493b866656e73b262f26197
Aldahir Zamora <ronn.zp.3@gmail.com>
2025-03-16 11:04:47 -0500

    agregar img, agregar estilos, componente guitarra

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,117 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
+<script setup>
+  import { ref, onMounted } from "vue";
+  import { db } from "./data/guitarras";
+  import Guitarra from "./components/Guitarra.vue"
+
+  const guitarras = ref([]);
+
+  onMounted(() => {
+    guitarras.value = db;
+  });
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <header class="py-5 header">
+        <div class="container-xl">
+            <div class="row justify-content-center justify-content-md-between">
+                <div class="col-8 col-md-3">
+                    <a href="index.html">
+                        <img class="img-fluid" src="/img/logo.svg" alt="imagen logo">
+                    </a>
+                </div>
+                <nav class="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+                    <div 
+                        class="carrito"
+                    >
+                        <img class="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+                        <div id="carrito" class="bg-white p-3">
+                            <p class="text-center">El carrito esta vacio</p>
+                            <table class="w-100 table">
+                                <thead>
+                                    <tr>
+                                        <th>Imagen</th>
+                                        <th>Nombre</th>
+                                        <th>Precio</th>
+                                        <th>Cantidad</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <img class="img-fluid" src="/img/guitarra_02.jpg" alt="imagen guitarra">
+                                        </td>
+                                        <td>SRV</td>
+                                        <td class="fw-bold">
+                                                $299
+                                        </td>
+                                        <td class="flex align-items-start gap-4">
+                                            <button
+                                                type="button"
+                                                class="btn btn-dark"
+                                            >
+                                                -
+                                            </button>
+                                                1
+                                            <button
+                                                type="button"
+                                                class="btn btn-dark"
+                                            >
+                                                +
+                                            </button>
+                                        </td>
+                                        <td>
+                                            <button
+                                                class="btn btn-danger"
+                                                type="button"
+                                            >
+                                                X
+                                            </button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+                            <p class="text-end">Total pagar: <span class="fw-bold">$899</span></p>
+                            <button class="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+                        </div>
+                    </div>
+                </nav>
+            </div><!--.row-->
 
-  <RouterView />
+            <div class="row mt-5">
+                <div class="col-md-6 text-center text-md-start pt-5">
+                    <h1 class="display-2 fw-bold">Modelo VAI</h1>
+                    <p class="mt-5 fs-5 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, possimus quibusdam dolor nemo velit quo, fuga omnis, iure molestias optio tempore sint at ipsa dolorum odio exercitationem eos inventore odit.</p>
+                    <p class="text-primary fs-1 fw-black">$399</p>
+                    <button 
+                        type="button"
+                        class="btn fs-4 bg-primary text-white py-2 px-5"
+                    >Agregar al Carrito</button>
+                </div>
+            </div>
+        </div>
+
+        <img class="header-guitarra" src="/img/header_guitarra.png" alt="imagen header">
+    </header>
+
+    <main class="container-xl mt-5">
+        <h2 class="text-center">Nuestra Colección</h2>
+
+        <div  class="row mt-5">
+            <Guitarra 
+                v-for="guitarra in guitarras"
+                :guitarra="guitarra"
+            />
+        </div>
+    </main>
+
+
+    <footer class="bg-dark mt-5 py-5">
+        <div class="container-xl">
+            <p class="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+    </footer>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
